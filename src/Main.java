07b6b234fb0ba90b3905127bd3ad6819cd9c6094
EMIR KALAJDŽIJA <ekalajdzij1@etf.unsa.ba>
2022-10-22 19:33:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Sat sati = new Sat(15,30,35);
+        sati.Ispisi();
+        sati.Sljedeci();
+        sati.Ispisi();
+        sati.PomjeriZa(-48);
+        sati.Ispisi();
+        sati.Postavi(0, 0, 0);
+        sati.Ispisi();
+    }
+}
